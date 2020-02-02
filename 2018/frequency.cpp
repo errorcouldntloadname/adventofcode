1625f2d83d67e2bd24fb47b46a9b5325d6626175
errorcouldntloadname <thomas.birkenheuer@mailbox.tu-dresden.de>
2020-02-02 18:44:32 +0100

    this is ugly ;(

--- 2018/frequency.cpp
+++ 2018/frequency.cpp
@@ -1,20 +1,31 @@
+#include <list>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <unordered_set>
 
 using namespace std;
 
 int main()
 {
+	unordered_set<int> lookup;
+	unordered_set<int> *lookpoint = &lookup;
+	list<int> datalist;
+	list<int>::iterator it;
 	int frequency = 0;	//stores the current frequency
 	int current = 0;	//current frequency read from data	
 	ifstream data;		//input data object
 	data.open("frequency.dat");
-
+	//read input into single list	
 	for(int i = 0; i < 994; i++) {
 		data>>current;
-		frequency = frequency + current;
-		cout<<frequency<<endl;	
+		datalist.push_back(current);
 	}
+
+	//update frequency
+	for(it = datalist.begin();it != datalist.end();it++)  {
+		frequency = frequency + *it;
+	}
+	
+	cout<<frequency<<endl;
 	return 0;
 }
